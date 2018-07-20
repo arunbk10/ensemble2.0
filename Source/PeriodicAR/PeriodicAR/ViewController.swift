8d551f3356b6ef7c56656df9b368414b7f654037
Deepa <deepa.bhat@ymedialabs.com>
2018-07-21 02:32:19 +0530

    Changed the position and color based on json file

--- Source/PeriodicAR/PeriodicAR/ViewController.swift
+++ Source/PeriodicAR/PeriodicAR/ViewController.swift
@@ -11,9 +11,10 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     var arrayElements = ["A", "H", "C", "B"]
+    var elementsArray: Array<City> = []
     
     var sessionConfiguration: ARWorldTrackingConfiguration = {
         let configuration = ARWorldTrackingConfiguration()
@@ -35,22 +36,40 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        
+        if let path = Bundle.main.path(forResource: "ElementList", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .dataReadingMapped)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let elements = jsonResult["elements"] as? [[String: Any]] {
+                    
+                    for i in 0...(elements.count)-1 {
+                        let element = elements[i]
+                        print(colorWithHexString(hex: element["color"] as! String))
+                        let city = City(name: element["name"] as! String, symbol: element["symbol"] as! String, color: colorWithHexString(hex: element["color"] as! String), xPosition: element["xpos"] as! CGFloat)
+                        elementsArray.append(city)
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        }
     }
     
     func createTable() {
-        for i in 0...3 {
+        for i in 0...(elementsArray.count - 1)  {
             let box = SCNBox(width: 1, height: 1, length: 0.01, chamferRadius: 0)
             let rectangleMaterial = SCNMaterial()
-            rectangleMaterial.diffuse.contents = UIColor.gray
+            rectangleMaterial.diffuse.contents = elementsArray[i].color
             box.materials = [rectangleMaterial]
             let boxNode = SCNNode(geometry: box)
             //Fix me
-            boxNode.position = SCNVector3(CGFloat(i)*1.05,0,-5)
-            boxNode.name = arrayElements[i]
+            boxNode.position = SCNVector3(elementsArray[i].xPosition,0,-5)
+            boxNode.name = elementsArray[i].symbol
             sceneView.scene.rootNode.addChildNode(boxNode)
             
             let textposition = SCNVector3Make(Float(i), 0,-1)
-            let textNode = addText(text: arrayElements[i], position: textposition,color: UIColor.gray)
+            let textNode = addText(text: elementsArray[i].symbol, position: textposition,color: elementsArray[i].color)
             let textMaterials = SCNMaterial()
             textMaterials.diffuse.contents = textNode
             box.materials = [textMaterials]
@@ -59,7 +78,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
     
     func addText(text: String, position: SCNVector3,color: UIColor)->SKScene {
         let skScene = SKScene(size: CGSize(width: 200, height: 200))
-        skScene.backgroundColor = UIColor.gray
+        skScene.backgroundColor = color
         let labelNode = SKLabelNode(text: text)
         labelNode.fontSize = 30
         labelNode.color = UIColor.red
@@ -73,17 +92,17 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-       
+        
         if ARWorldTrackingConfiguration.isSupported {
             self.sceneView.automaticallyUpdatesLighting = true
             sessionConfiguration.isLightEstimationEnabled = true
             self.sceneView.autoenablesDefaultLighting = true
             self.sceneView.session.run(sessionConfiguration)
             UIApplication.shared.isIdleTimerDisabled = true
-          
+            
             createTable()
         }
-  
+        
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -96,16 +115,16 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     // MARK: - ARSCNViewDelegate
-
+    
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
         return node
     }
-
+    
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -132,5 +151,29 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
                 print(node.name ?? "")
             }
         }
+    }
+    
+    func colorWithHexString (hex:String) -> UIColor {
+        
+        var cString = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString = (cString as NSString).substring(from: 1)
+        }
+        
+        if (cString.characters.count != 6) {
+            return UIColor.gray
+        }
+        
+        let rString = (cString as NSString).substring(to: 2)
+        let gString = ((cString as NSString).substring(from: 2) as NSString).substring(to: 2)
+        let bString = ((cString as NSString).substring(from: 4) as NSString).substring(to: 2)
+        
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
+        Scanner(string: rString).scanHexInt32(&r)
+        Scanner(string: gString).scanHexInt32(&g)
+        Scanner(string: bString).scanHexInt32(&b)
+        
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(1))
     }
 }
